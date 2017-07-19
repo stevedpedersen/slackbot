f83c35db0ef5564556517d1f8f3f285b7df9168d
Steve Pedersen <spedersen@spedersen16.dev.at.sfsu.edu>
2017-07-19 16:50:36 -0700

    slackApi module to get key based on filename.

--- iris/bin/run.js
+++ iris/bin/run.js
@@ -5,15 +5,20 @@ const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
-const slackToken = require('../server/api-key').key;
+const slackApi = require('../server/slackApi');
+const slackToken = slackApi.init('iris-api-key.txt');
 const slackLogLevel = 'verbose';
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
-rtm.start();
+if (slackToken) {
+	const rtm = slackClient.init(slackToken, slackLogLevel);
+	rtm.start();
 
-// when connected to Slack we call server.listen
-slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+	// when connected to Slack we call server.listen
+	slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
 
-server.on('listening', function() {
-    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
-});
+	server.on('listening', function() {
+	    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+	});
+} else {
+	console.log('Could not locate the Slack API key.');
+}
