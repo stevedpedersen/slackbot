652e004611543901f2c7d6ea99063c61eea66cb7
Steve Pedersen <spedersen@spedersen16.dev.at.sfsu.edu>
2017-07-20 16:46:18 -0700

    Included google timezone api to get time based on geocodes.

--- iris-time/server/service.js
+++ iris-time/server/service.js
@@ -3,19 +3,52 @@
 const express = require('express');
 const service = express();
 const request = require('superagent');
+const moment = require('moment');
 
-const googleToken = require('./apiKeys').get('google-api-key');
+const geoToken = require('./apiKeys').get('google-geo-api-key');
+const timeToken = require('./apiKeys').get('google-timezone-api-key');
+
+// https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
 
 service.get('/service/:location', (req, res, next) => {
 	request.get(
-		`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${googleToken}`,
+		`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${geoToken}`,
 		(err, response) => {
 			if (err) {
 				console.log(err);
 				return res.sendStatus(500);
 			}
 
-			res.json(response.body.results[0].geometry.location);
+			const location = response.body.results[0].geometry.location;
+			const timestamp = +moment().format('X');
+
+			request.get(
+				`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=${timeToken}`,
+				(err, response) => {
+					if (err) {
+						console.log(err);
+						return res.sendStatus(500);
+					}
+
+					const result = response.body;
+					const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
+
+					res.writeHead(200, {'Content-Type': 'text/html'});
+
+					res.end(`
+						<!DOCTYPE html>
+						<html>
+							<head>
+								<title>Time in ${req.params.location}</title>	
+							</head>
+							<body>
+								<h1>Time in ${req.params.location}</h1>
+								<p>${timeString}</p>
+							</body>
+						</html>
+					`);
+				}
+			);
 		}
 	);
 
