46d3889054138287c28212ae2474cb34bc598b1b
Steve Pedersen <spedersen@spedersen16.dev.at.sfsu.edu>
2017-07-20 15:44:50 -0700

    Refactored Wit code to use 'intent' object routing.

--- iris/server/slackClient.js
+++ iris/server/slackClient.js
@@ -13,25 +13,37 @@ function handleOnAuthenticated(rtmStartData) {
 }
 
 function handleOnMessage(message) {	
-	nlp.ask(message.text, (err, res) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
 
-		if (!res.intent) {
-			return rtm.sendMessage('Sorry, I do not know what you are talking about', message.channel);
-		} else if (res.intent[0].value === 'time' && res.location) {
-			return rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, message.channel);
-		} else {
-			console.log(res);
-			return rtm.sendMessage('Sorry, I do not know what you are talking about', message.channel);
-		}
+	if (message.text.toLowerCase().includes('iris')) {
+		nlp.ask(message.text, (err, res) => {
+			if (err) {
+				console.log(err);
+				return;
+			}
+
+			try {
+				if (!res.intent || !res.intent[0] || !res.intent[0].value) {
+					throw new Error('Could not extract intent.');
+				}
+
+				const intent = require('./intents/' + res.intent[0].value + 'Intent');
+
+				intent.process(res, function(error, response) {
+					if (error) {
+						console.log(error.message);
+						return;
+					}
+					return rtm.sendMessage(response, message.channel);
+				});
 
 
-
-		rtm.sendMessage("Sorry, I did not understand", message.channel);		
-	});	
+			} catch (err) {
+				console.log(err);
+				console.log(res);
+				rtm.sendMessage("Sorry, I don't know what you're talking about!", message.channel);
+			}	
+		});			
+	}
 }
 
 function addAuthenticatedHandler(rtm, handler) {
